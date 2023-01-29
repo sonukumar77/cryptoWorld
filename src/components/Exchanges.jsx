eee458beb91b9802a76701f5f97be39e2fc2fa04
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2023-01-29 14:01:42 +0530

    Update Exchanges.jsx

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -4,6 +4,7 @@ const Exchanges = () => {
     return(
         <div>
        Exchanges
+        <p> This API is not giving access for FREE So i unable to do with this page and getting error <b>This endpoint is disabled for your subscription.</b></p>
     </div>
     )
 }
